3c00da8344bd9f27c70c03831bf482fec654b049
YosCiiCable <YosCiiCable@gmail.com>
2023-02-22 15:36:53 +0900

    ポップアップ後の処理追加

---
+++ BookMarkLet/BML-Login2Course.js
@@ -0,0 +1,12 @@
+javascript: (
+	BtnLogin.click();
+	LbtSubCourseLink_1.click(); 
+	GoToStUnitList_Click('PWH_L03');
+
+	ShowLearnPage('PWH_L03_U001-1','U001','09', '', 'UNIT001','3', '&STCnt1=');
+
+	document.querySelector(".nan-button-action").click();
+
+	// ShowLearnPage('PWH_L03_U002-1','U002','09', '', 'UNIT002','3', '&STCnt1=');
+	// ShowLearnPage('PWH_L03_U00*-1','U00*','09', '', 'UNIT00*','3', '&STCnt1=');
+)();
