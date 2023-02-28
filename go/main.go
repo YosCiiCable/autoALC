ac59d87baa756ee7389bd6f90199e6d782b98a39
YosCiiCable <YosCiiCable@gmail.com>
2023-02-28 22:27:56 +0900

    dockerでselenium建てるように

--- go/main.go
+++ go/main.go
@@ -1,10 +1,37 @@
+
 package main
 
 import (
-	"fmt"
-	"github.com/sclevine/agouti"
+    "github.com/sclevine/agouti"
+    "log"
+    "time"
 )
 
 func main() {
-	fmt.Printf("ok")
+    // ブラウザはChromeを指定して起動
+    driver := agouti.ChromeDriver(agouti.Browser("chrome"))
+    if err := driver.Start(); err != nil {
+        log.Fatalf("Failed to start driver:%v", err)
+    }
+    defer driver.Stop()
+
+    page, err := driver.NewPage()
+    if err != nil {
+        log.Fatalf("Failed to open page:%v", err)
+    }
+    // ログインページに遷移
+    if err := page.Navigate("https://qiita.com/login"); err != nil {
+        log.Fatalf("Failed to navigate:%v", err)
+    }
+    // ID, Passの要素を取得し、値を設定
+    identity := page.FindByID("identity")
+    password := page.FindByID("password")
+    identity.Fill("Your Id Here.")
+    password.Fill("Your Passowrd Here.")
+    // formをサブミット
+    if err := page.FindByClass("loginSessionsForm_submit").Submit(); err != nil {
+        log.Fatalf("Failed to login:%v", err)
+    }
+    // 処理完了後、3秒間ブラウザを表示しておく
+    time.Sleep(3 * time.Second)
 }
