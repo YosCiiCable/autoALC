a1edf54852b2e86dfe6c11727e052e23d8715993
YosCiiCable <YosCiiCable@gmail.com>
2023-03-28 23:52:51 +0900

    久しぶり

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # autoALC
 ALCを自動でしてくれるツールを作りたい人生だった…。
 
-# 実行関係
-## エラー
+# エラー
 ~~~
 2023/03/07 18:59:10 err0: English
 ~~~
 というような表示で終了した場合は、エラーです。
 入力ミスの場合もありますし、実行上のエラーの場合もあります。
 再度最初から行っても同じエラーの場合は、開発者に連絡ください。
+
+# Docker を使う
 ## 起動
 go/ で以下のコードを分けて実行
 ~~~
@@ -20,6 +21,34 @@ go/ で以下のコードを実行
 ~~~
 docker compose down --rmi all --volumes --remove-orphans
 ~~~
+
+# build する
+## Windows (64bit amd)
+~~~
+GOOS=windows GOARCH=amd64 go build -o main.exe main.go
+~~~
+## GOARCHに利用可能なもの
+- 386
+- amd64
+- arm
+
+## Linux (64bit intel)
+~~~
+GOOS=linux GOARCH=arm64 go build -o main.exe main.go
+~~~
+## GOARCHに利用可能なもの
+- 386
+- amd64
+- arm
+- arm64
+- mips
+- mips64
+- mips64le
+- mipsle
+- ppc64
+- ppc64le
+- riscv64
+- s390x
 
 # ディレクトリの説明
 ## archive/
--- go/main.go
+++ go/main.go
@@ -2,104 +2,233 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"time"
+
+	"github.com/chromedp/cdproto/input"
+	"github.com/chromedp/cdproto/target"
 	"github.com/chromedp/chromedp"
+
 	"github.com/manifoldco/promptui"
 )
 
 func main() {
-	// level0: chromeのインスタンス作成(上はデバックON)
-	ctx, cancel := chromedp.NewContext(context.Background(), chromedp.WithDebugf(log.Printf))
-	//ctx, cancel := chromedp.NewContext(context.Background(),)
-	defer cancel()
+	// level0: chromeのインスタンス作成
+	ctx, _ := chromedp.NewContext(context.Background()) /*
+		// level0-debug1: ログあり でインスタンス作成
+		ctx, _ := chromedp.NewContext(context.Background(), chromedp.WithDebugf(log.Printf)) /*
+		// level0-debug2: no headless でインスタンス作成
+		opts := append(chromedp.DefaultExecAllocatorOptions[:],
+			chromedp.Flag("headless", false),
+			chromedp.Flag("enable-automation", false),
+		)
+		allocCtx, _ := chromedp.NewExecAllocator(context.Background(), opts...)
+		ctx, _ := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
+		//*/
+	//defer cancel()
 
 	// level1: login
-	if err := chromedp.Run(ctx,
-		login(),
-	); err != nil {
-		log.Fatal("err1: login")
-	}
-	// level1-2: エラーチェック
-//	loginErrorMessage := "hoge"
+	login(ctx)
+
+	// level2: 遷移チェック
 	if err := chromedp.Run(ctx,
 		chromedp.Click(`//*[@id="LbtMaRePassword"]/font`, chromedp.AtLeast(0)),
-//		chromedp.Text(`"//*[@id="form1"]/div[2]/div[2]/div[5]/div/span[2]/span"`, &loginErrorMessage, chromedp.AtLeast(0)),
 	); err == nil {
-		log.Fatal("err1: Login Information Error")
-//	} else if loginErrorMessage != "hoge" {
-//		log.Fatal("err1: Login Information Error")
+		log.Fatal("err2: Transition check fails")
 	}
 	fmt.Println("ログイン完了")
 
-	// level2: Unitsに移動
-	fmt.Println("ユニット一覧に移動開始…")
+	// level3: Unitsに移動
+	fmt.Println("ユニット一覧に移動開始………")
 	if err := chromedp.Run(ctx,
 		chromedp.Click(`//*[@id="LbtSubCourseLink_1"]`, chromedp.NodeVisible),
 		chromedp.Click(
 			`//*[@id="DivAllSubCourseTable"]/table/tbody/tr/td[2]/table/tbody/tr[3]/td[1]/a`,
 			chromedp.NodeVisible,
 		),
+		chromedp.WaitVisible(`//*[@id="nan-contents"]/div[1]/div/label/font`),
 	); err != nil {
-		log.Fatal("err2: move to Units")
+		log.Fatal("err3: Failed to move units")
 	}
 	fmt.Println("ユニット一覧移動完了")
 
-	// level3: Input開始
-//	fmt.Println("種別:インプット をやりますか？")
-	if yesNo("種別:インプット をやりますか？") {
-		//if err := chromedp.Run(ctx, inputer(), ); err != nil { log.Fatal("err3: run Input") }
-		fmt.Println("Input開始")
+	// level4: Input開始
+	if yesNo("インプット をやりますか？") {
+		fmt.Println("インプット開始………")
+		for i, k := 2, 0; i < 220; i += 2 {
+			k++
+			inputSelector(ctx, i, (i-(k/5))/2)
+			debugPic(ctx)
+			if k%5 == 0 {
+				i++
+			}
+		}
 	}
 
-	// level4: Output開始
-	fmt.Println("種別:ドリル をやりますか？")
+	// level5: Output開始
+	if yesNo("ドリル をやりますか？") {
+		fmt.Println("ドリル開始………")
+	}
 }
 
-func login() chromedp.Tasks {
-	var studentNum, password string
-	fmt.Print("sを含めた学籍番号:")
+func login(ctx context.Context) {
+	var studentNum, passwd string
+	fmt.Print("sを含めた学籍番号: ")
 	fmt.Scan(&studentNum)
-	fmt.Print("ALCのパスワード:")
-	fmt.Scan(&password)
-	fmt.Println("ログイン開始…")
+	passwd = passwdInputer("ALCのパスワード")
+	fmt.Println("ログイン開始………")
 
-	return chromedp.Tasks{
+	if err := chromedp.Run(ctx,
 		chromedp.Navigate("https://nanext.alcnanext.jp/anetn/Student/stlogin/index/nit-ariake/"),
 
-		chromedp.WaitVisible(`//*[@id="Password"]`),
-		chromedp.SendKeys(`//*[@id="AccountId"]`, studentNum),
-		chromedp.SendKeys(`//*[@id="Password"]`, password),
+		chromedp.Click(`//*[@id="AccountId"]`, chromedp.NodeVisible),
+		input.InsertText(studentNum),
+		chromedp.Click(`//*[@id="Password"]`, chromedp.NodeVisible),
+		input.InsertText(passwd),
 		chromedp.Click(`//*[@id="BtnLogin"]`, chromedp.NodeVisible),
+	); err != nil {
+		log.Fatal("err1: Failed login")
 	}
 }
 
-//func inputer() chromedp.Tasks {
-//	fmt.Println("入力開始")
-//	return chromedp.Tasks{
-//		chromedp.Click(`//`, chromedp.NodeVisible),
-//	}
-//}
+func inputSelector(ctx context.Context, i int, unitNum int) {
+	iXPath := "//*[@id=\"nan-contents\"]/div[7]/div/table/tbody/tr[" + strconv.Itoa(i)
+	// if naming, iXPath + "]/td[4]"	is iStatusXPath
+	// if naming, iXPath + "]/td[3]/a"	is iClickXPath
+	iStatusText := "init"
+	if err := chromedp.Run(ctx,
+		chromedp.Text(iXPath+"]/td[4]", &iStatusText),
+	); err != nil {
+		log.Fatal("err@inputSelector-1: Failed to get the input-unit status")
+	}
+
+	if iStatusText == "未学習 / Not studied" || iStatusText == "参照のみ / Only opened" || iStatusText == "学習中 / In progress" {
+		if err := chromedp.Run(ctx,
+			chromedp.Click(iXPath+"]/td[3]/a", chromedp.NodeVisible),
+		); err != nil {
+			log.Fatal("err@inputSelector-2: Failed to click the input-unit")
+		}
+		inputer(ctx, unitNum)
+	} else if iStatusText == "修了 / Completed" {
+		fmt.Printf("修了済み Unit%d\n", unitNum)
+	} else {
+		fmt.Println("warning  : 続行可能な程度の例外発生、続行します。")
+		fmt.Println("---------: よければ以下の1行を開発者にお知らせください。")
+		fmt.Println("warn@inputSelector-3:Exception at last of inputer:" + iStatusText)
+	}
+}
+
+func inputer(ctx context.Context, unitNum int) {
+	var homeID, targetID target.ID = "", ""
+	fmt.Printf("Unit%dを処理中…\n", unitNum)
+	for i := 0; i < 1; {
+		targets, err := chromedp.Targets(ctx)
+		if err != nil {
+			log.Fatal("err@inputer-1: Failed to make a new target")
+		}
+		for _, t := range targets {
+			if t.Type == "page" && t.URL != "about:blank" {
+				if homeID == "" {
+					homeID = t.TargetID
+				}
+				if t.URL != "https://nanext.alcnanext.jp/anetn/Student/StUnitList#" {
+					targetID = t.TargetID
+					i++
+				}
+			}
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	if homeID == "" {
+		log.Fatal("err@inputer-2: The homeID is nil")
+		return
+	}
+	if targetID == "" {
+		log.Fatal("err@inputer-3: The targetID is nil")
+		return
+	}
+	ctx, _ = chromedp.NewContext(ctx, chromedp.WithTargetID(targetID))
+
+	if err := chromedp.Run(ctx,
+		chromedp.Click(`//*[@id="nan-contents-cover-buttons"]/div/div[1]/button/span[1]`, chromedp.NodeVisible),
+		chromedp.Click(`.ui-dialog-buttonset > button:nth-child(1)`, chromedp.NodeVisible),
+		chromedp.Click(`//*[@id="nan-toolbox-footer"]/button`, chromedp.NodeVisible),
+		chromedp.Click(`.ui-dialog-buttonset > button:nth-child(1)`, chromedp.NodeVisible),
+	); err != nil {
+		log.Fatal("err@inputer-4: Failed to click element in input")
+	}
+
+	ctx, _ = chromedp.NewContext(ctx, chromedp.WithTargetID(homeID))
+
+	fmt.Printf("完了: Unit%d\n", unitNum)
+}
 
 func yesNo(labelMessage string) bool {
 	prompt := promptui.Select{
 		Label: labelMessage,
 		Items: []string{"Yes", "No"},
 	}
+
 	_, result, err := prompt.Run()
 	if err != nil {
-		log.Fatalf("Prompt failed %v\n", err)
+		log.Fatal("err@yesNo: Failed to run prompt")
 	}
+
 	return result == "Yes"
 }
 
-func debug() {
+func passwdInputer(labelMessage string) string {
+	validate := func(input string) error {
+		if len(input) < 6 {
+			return errors.New("パスワードは6~20文字の範囲の筈です…")
+		} else if len(input) > 20 {
+			return errors.New("パスワードは6~20文字の範囲の筈です…")
+		}
+		return nil
+	}
+
+	prompt := promptui.Prompt{
+		Label:    labelMessage,
+		Validate: validate,
+		Mask:     '*',
+	}
+
+	passwd, err := prompt.Run()
+	if err != nil {
+		log.Fatal("err@passwdInputer: Failed to run prompt")
+	}
+
+	return passwd
+}
+
+func debugURL(ctx context.Context) {
+	var url string
+	if err := chromedp.Run(ctx,
+		chromedp.Location(&url),
+	); err != nil {
+		log.Fatal("err@debugURL: Failed to location url")
+	}
+	fmt.Printf("debugURL: %s\n", url)
+
+}
+
+func debugPic(ctx context.Context) {
 	var buf []byte
-	ctx, cancel := chromedp.NewContext(context.Background(), chromedp.WithDebugf(log.Printf))
-	defer cancel()
 	// debug level1: スクショ撮影
-	if err := chromedp.Run(ctx, chromedp.FullScreenshot(&buf, 90), ); err != nil { log.Fatal("errD1: capture a screenshot") }
+	if err := chromedp.Run(ctx,
+		chromedp.FullScreenshot(&buf, 90),
+	); err != nil {
+		log.Fatal("err@debugPic-1: Failed to capture a screenshot")
+	}
 	// debug level2: スクショ出力
-	if err := os.WriteFile("fullScreenshot.png", buf, 0o644); err != nil { log.Fatal("errD2: output the screenshot") }
+	if err := os.WriteFile(
+		"fullScreenshot.png", buf, 0o644,
+	); err != nil {
+		log.Fatal("err@debugPic-2: Failed to output the screenshot")
+	}
 }
